da4552b353b1d1a3ecd15866cba4aef7c2853855
Jeetesh Kumar Shaw <jeeteshshaw@gmail.com>
2020-09-19 09:10:08 +0530

    Footer Added

--- src/App.js
+++ src/App.js
@@ -1,22 +1,72 @@
-import React, {  } from 'react'
+import React, { } from 'react'
 import './App.css'
-
-import { BrowserRouter as Router, Switch, Route} from "react-router-dom"
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom"
 import Category from "./components/Category/Category"
 import Navbar from './components/Navbar/Navbar'
+import { Grid, makeStyles } from '@material-ui/core'
 
+
+const useStyle = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+    // background: "red"
+  },
+}))
 
 function App() {
 
   // const [spacing, setSpacing] = useState(2);
-  
+  const classes = useStyle()
+
   return (
     <Router>
       <div className="background"></div>
       <Navbar />
-      <Switch >
-        <Route path="/" component={Category} />
-      </Switch>
+      <main></main>
+      <footer>
+        <Grid container justify="space-around" className={classes.root} spacing={2}>
+          <Grid className="footer__box" item xs={4} md={2}>
+            <ul className="footer__list">
+              <li><h2 id="footer-logo" className="">Humti Dumti</h2></li>
+              <li>Enjoy Latest Movies and Web Series.</li>
+            </ul>
+          </Grid>
+          <Grid className="footer__box" item xs={4} md={2}>
+            <div className="footer__menu">
+              <ul className="footer__list">
+                <li>Explore</li>
+                <li>Home</li>
+                <li>Category</li>
+                <li>About</li>
+                <li>Support</li>
+                <li>Report</li>
+              </ul>
+            </div>
+          </Grid>
+
+          <Grid item md={2} xs={4} className="footer__box">
+            <div className="footer__social">
+              <ul className="footer__list">
+                <li>Follow</li>
+                <li>Instagram</li>
+                <li>Twitter</li>
+                <li>Facebook</li>
+                <li>Youtube</li>
+              </ul>
+            </div>
+          </Grid>
+          <Grid item md={2} xs={4} className="footer__box">
+            <div className="footer__social" >
+              <ul className="footer__list">
+                <li>Legal</li>
+                <li>Terms</li>
+                <li>Privacy</li>
+              </ul>
+            </div>
+          </Grid>
+
+        </Grid>
+      </footer>
     </Router>
   );
 }
---
+++ src/components/Homepage/Homepage.js
@@ -0,0 +1,12 @@
+  <Swiper
+      spaceBetween={50}
+      slidesPerView={3}
+      onSlideChange={() => console.log('slide change')}
+      onSwiper={(swiper) => console.log(swiper)}
+    >
+      <SwiperSlide>Slide 1</SwiperSlide>
+      <SwiperSlide>Slide 2</SwiperSlide>
+      <SwiperSlide>Slide 3</SwiperSlide>
+      <SwiperSlide>Slide 4</SwiperSlide>
+      ...
+    </Swiper>
